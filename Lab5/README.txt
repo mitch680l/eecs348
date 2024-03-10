cea6b9c2c27593a5ca2882108720ac6a09b1c488
Mitch <yolobob115@gmail.com>
2024-03-10 18:25:50 -0500

    pt1 done

---
+++ Lab5/README.txt
@@ -0,0 +1,5 @@
+part 1 uses fstream to read files
+
+I believe a relative file path works.
+However its easier to make the datafile the same folder as the code.
+Then you simply input the filename into the input prompt.
---
+++ Lab5/part1.cpp
@@ -0,0 +1,107 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+int ring12(int n);
+
+int main() {
+  std::string months[] = {"January  ", "February ", "March    ", "April    ",
+                          "May      ", "June     ", "July     ", "August   ",
+                          "September", "October  ", "November ", "December "};
+  double report[12];
+  float minNum = 0.0;
+  float maxNum = 0.0;
+  float avgNum = 0.0;
+  float sumNum = 0.0;
+  std::ifstream myFile;
+  std::string fileName;
+  std::cout << "filepath for textfile: " << '\n' << '\n';
+  std::cin >> fileName;
+
+  myFile.open(fileName);
+
+  for (int i = 0; i < 12; i++) {
+    float k;
+    myFile >> k;
+    report[i] = k;
+    sumNum += k;
+  }
+  avgNum = sumNum / 12;
+  minNum = report[0];
+  maxNum = report[0];
+
+  for (int i = 0; i < 12; i++) {
+    if (report[i] > maxNum) {
+      maxNum = report[i];
+    }
+    if (report[i] < minNum) {
+      minNum = report[i];
+    }
+  }
+
+  std::cout << '\n' << "Monthly sales report for 2022:" << '\n';
+  for (int i = 0; i < 12; i++) {
+    std::cout << std::setw(8) << months[i];
+    std::cout << " $" << std::fixed << std::setprecision(2) << report[i]
+              << '\n';
+  }
+
+  std::cout << '\n'
+            << std::left << std::setw(15) << "Average: "
+            << " $" << std::fixed << std::setprecision(2) << avgNum << '\n';
+  std::cout << std::left << std::setw(15) << "Highest Sales: "
+            << " $" << std::fixed << std::setprecision(2) << maxNum << '\n';
+  std::cout << std::left << std::setw(15) << "Lowest Sales: "
+            << " $" << std::fixed << std::setprecision(2) << minNum << '\n';
+
+  for (int i = 0; i < 8; i++) {
+    float six_month_sum = 0.0;
+    for (int k = 0; k < 6; k++) {
+      int month = i + k;
+      six_month_sum += report[ring12(month)];
+    }
+    std::cout << '\n' << std::setw(12) << std::left << months[i];
+    std::cout << '-';
+    std::cout << std::setw(12) << std::internal << months[ring12(i + 5)];
+    std::cout << std::setw(12) << " $" << std::fixed << std::setprecision(2)
+              << six_month_sum / 6 << '\n';
+  }
+
+  std::string sortedMonthes[12] = {"January  ", "February ", "March    ",
+                                   "April    ", "May      ", "June     ",
+                                   "July     ", "August   ", "September",
+                                   "October  ", "November ", "December "};
+  double sortedReport[12];
+
+  for (int i = 0; i < 12; i++) {
+    sortedReport[i] = report[i];
+  }
+  for (int i = 0; i < 12; i++) {
+    for (int k = i; k < 11; k++) {
+      if (sortedReport[k] > sortedReport[k + 1]) {
+        int temp = sortedReport[k];
+        sortedReport[k] = sortedReport[k + 1];
+        sortedReport[k + 1] = temp;
+
+        std::string tempM = sortedMonthes[k];
+        sortedMonthes[k] = sortedMonthes[k + 1];
+        sortedMonthes[k + 1] = tempM;
+      }
+    }
+  }
+  std::cout << '\n' << "Sales Report (Highest to Lowest):\n" << '\n';
+  std::cout << std::setw(10) << "Month"
+            << " Sales" << std::endl;
+  for (int i = 11; i >= 0; i--) {
+    std::cout << std::setw(10) << months[i] << " $" << std::fixed
+              << std::setprecision(2) << report[i] << std::endl;
+  }
+}
+
+int ring12(int n) {
+  if (n >= 12) {
+    return n - 12;
+  } else {
+    return n;
+  }
+}
