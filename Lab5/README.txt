980c16995da829713e15eba84685c60bc9a2f9cc
Mitch <yolobob115@gmail.com>
2024-03-11 02:26:12 -0500

    small bugs fixed, added a few general comments and formating

--- Lab5/README.txt
+++ Lab5/README.txt
@@ -1,5 +1,3 @@
 part 1 uses fstream to read files
 
-I believe a relative file path works.
-However its easier to make the datafile the same folder as the code.
-Then you simply input the filename into the input prompt.
+*make sure to have input text file in same folder as executable/code*
--- Lab5/part1.cpp
+++ Lab5/part1.cpp
@@ -15,12 +15,13 @@ int main() {
   float sumNum = 0.0;
   std::ifstream myFile;
   std::string fileName;
-  std::cout << "filepath for textfile: " << '\n' << '\n';
+  std::cout << "name for textfile: " << '\n' << '\n';
   std::cin >> fileName;
 
   myFile.open(fileName);
 
   for (int i = 0; i < 12; i++) {
+   //this loop uses fstream to read the data
     float k;
     myFile >> k;
     report[i] = k;
@@ -30,6 +31,7 @@ int main() {
   minNum = report[0];
   maxNum = report[0];
 
+  //basic search algorithm for min and max
   for (int i = 0; i < 12; i++) {
     if (report[i] > maxNum) {
       maxNum = report[i];
@@ -39,6 +41,7 @@ int main() {
     }
   }
 
+  //outputs baisc sales report
   std::cout << '\n' << "Monthly sales report for 2022:" << '\n';
   for (int i = 0; i < 12; i++) {
     std::cout << std::setw(8) << months[i];
@@ -46,6 +49,7 @@ int main() {
               << '\n';
   }
 
+  //outputs for average, max and min sales
   std::cout << '\n'
             << std::left << std::setw(15) << "Average: "
             << " $" << std::fixed << std::setprecision(2) << avgNum << '\n';
@@ -54,12 +58,15 @@ int main() {
   std::cout << std::left << std::setw(15) << "Lowest Sales: "
             << " $" << std::fixed << std::setprecision(2) << minNum << '\n';
 
+
   for (int i = 0; i < 8; i++) {
     float six_month_sum = 0.0;
     for (int k = 0; k < 6; k++) {
+      //finds 6 month interval by using a ring structure where 12 and above loops around(in a ring)
       int month = i + k;
       six_month_sum += report[ring12(month)];
     }
+    //rolling 6 month sales report outputs
     std::cout << '\n' << std::setw(12) << std::left << months[i];
     std::cout << '-';
     std::cout << std::setw(12) << std::internal << months[ring12(i + 5)];
@@ -73,22 +80,27 @@ int main() {
                                    "October  ", "November ", "December "};
   double sortedReport[12];
 
+  //copies report to sortedReport to keep them seperated
   for (int i = 0; i < 12; i++) {
     sortedReport[i] = report[i];
   }
+
+  //very basic bubble sort
   for (int i = 0; i < 12; i++) {
     for (int k = i; k < 11; k++) {
       if (sortedReport[k] > sortedReport[k + 1]) {
+
         int temp = sortedReport[k];
         sortedReport[k] = sortedReport[k + 1];
         sortedReport[k + 1] = temp;
-
+        //sort the month strings at the same time
         std::string tempM = sortedMonthes[k];
         sortedMonthes[k] = sortedMonthes[k + 1];
         sortedMonthes[k + 1] = tempM;
       }
     }
   }
+  //sorted sales summery outputs
   std::cout << '\n' << "Sales Report (Highest to Lowest):\n" << '\n';
   std::cout << std::setw(10) << "Month"
             << " Sales" << std::endl;
