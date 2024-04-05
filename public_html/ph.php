14e20313b77a832600e07ab982f3b325a3ecaa80
Mitchell Leonard <143823699+mitch680l@users.noreply.github.com>
2024-04-05 14:40:10 -0500

    added website files for lab7

---
+++ public_html/ph.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <table border="1">
+            <tbody >
+                <?php
+                $x = $_POST["fname"];
+                for($i = 1; $i < $x + 1; $i += 1) {
+                    echo "<tr>";
+                    for ($j = 1; $j < $x + 1; $j += 1) {
+                        
+                        $result = $j * $i;
+                        echo "<td> $result </td>";
+                    }
+                    echo "</tr>";
+                } 
+
+                ?>
+            </tbody>
+        </table>
+    </body>
+</html>
